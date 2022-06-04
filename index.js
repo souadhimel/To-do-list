e63c1a7c95061060829927d4b4c083e6a9055721
souadhimel <souad.anam@gmail.com>
2022-06-04 14:05:32 +0600

    title added

---
+++ index.js
@@ -0,0 +1,27 @@
+
+
+
+document.getElementById('button_add').addEventListener('click',
+function() {
+    const inputValue=document.getElementById('field_input').value;
+
+if (inputValue=='') {
+  alert('Please enter a value');  
+}
+else{
+
+    const mainContainer=document.getElementById('table_container')
+    const tableContainer=document.createElement('tr');
+    tableContainer.innerHTML=` <th scope="row">1</th>
+    <td>Mark</td>
+    <td>Button</td>`
+    mainContainer.appendChild(tableContainer);
+
+    document.getElementById('field_input').value=""
+
+
+
+
+}
+
+})
