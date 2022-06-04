da9550be2291c5c964ff8095cd40a0e593b89dec
souadhimel <souad.anam@gmail.com>
2022-06-04 14:47:38 +0600

    button created

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-
+let count=0;
 
 
 document.getElementById('button_add').addEventListener('click',
 function() {
+    count++
     const inputValue=document.getElementById('field_input').value;
 
 if (inputValue=='') {
@@ -12,9 +13,9 @@ else{
 
     const mainContainer=document.getElementById('table_container')
     const tableContainer=document.createElement('tr');
-    tableContainer.innerHTML=` <th scope="row">1</th>
-    <td>Mark</td>
-    <td>Button</td>`
+    tableContainer.innerHTML=` <th className="text-center">${count}</th>
+    <td>${inputValue}</td>
+    <td><button className="btn btn-danger">Delete</button><button className="btn btn-primary ms-3">Done</button></td>`
     mainContainer.appendChild(tableContainer);
 
     document.getElementById('field_input').value=""
