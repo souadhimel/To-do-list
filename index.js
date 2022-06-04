5595774beb14a92a08ba3cc8d8d60f880b672493
souadhimel <souad.anam@gmail.com>
2022-06-04 14:07:48 +0600

    if else created

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ if (inputValue=='') {
 }
 else{
 
-    // const mainContainer=document.getElementById('table_container')
-    // const tableContainer=document.createElement('tr');
-    // tableContainer.innerHTML=` <th scope="row">1</th>
-    // <td>Mark</td>
-    // <td>Button</td>`
-    // mainContainer.appendChild(tableContainer);
+    const mainContainer=document.getElementById('table_container')
+    const tableContainer=document.createElement('tr');
+    tableContainer.innerHTML=` <th scope="row">1</th>
+    <td>Mark</td>
+    <td>Button</td>`
+    mainContainer.appendChild(tableContainer);
 
     document.getElementById('field_input').value=""
 
